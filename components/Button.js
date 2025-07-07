8b26f71a817ff5580f3fcdaa4d7943b2c1ac5632
“bigred1983” <robert.clifford-jones@contentful.com>
2025-07-07 10:10:52 +0100

    button image size

--- components/Button.js
+++ components/Button.js
@@ -1,6 +1,7 @@
 export default function Button({ button }) {
   if (!button?.fields) return null;
-const label = button.fields.label || "Click Here";
+
+  const label = button.fields.label || "Click Here";
   const url = button.fields.destinationUrl || "#";
 
   const imageUrl = button.fields.iconOrImage?.fields?.file?.url
@@ -14,14 +15,14 @@ const label = button.fields.label || "Click Here";
         <img
           src={imageUrl}
           alt={label}
-          className="mx-auto mb-4 w-32 h-32 object-contain"
+          className="mx-auto mb-4 w-40 h-40 max-w-full object-contain"
         />
-     )}
+      )}
 
       {/* Button */}
       <a
         href={url}
-      className="inline-block border-2 border-black px-8 py-4 text-lg font-semibold text-black bg-white rounded-lg shadow hover:bg-gray-100 transition"
+        className="inline-block border-2 border-black px-8 py-4 text-lg font-semibold text-black bg-white rounded-lg shadow hover:bg-gray-100 transition"
       >
         {label}
       </a>
